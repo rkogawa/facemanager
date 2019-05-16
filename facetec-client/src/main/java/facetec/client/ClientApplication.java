e06f8068c7a2f3036545ba4c2ab220637f77c0e1
rkogawa <rkogawa@gmail.com>
2019-05-16 03:31:58 -0300

    Ajuste para gerar cliente com configuracao

--- facetec-client/src/main/java/facetec/client/ClientApplication.java
+++ facetec-client/src/main/java/facetec/client/ClientApplication.java
@@ -22,14 +22,14 @@ public class ClientApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        JFrame frame = new JFrame("Spring Boot Swing App");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
-        JPanel panel = new JPanel(new BorderLayout());
-        JTextField text = new JTextField("Spring Boot can be used with Swing apps");
-        panel.add(text, BorderLayout.CENTER);
-        frame.setContentPane(panel);
-        frame.setVisible(true);
+//        JFrame frame = new JFrame("Spring Boot Swing App");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(300, 300);
+//        JPanel panel = new JPanel(new BorderLayout());
+//        JTextField text = new JTextField("Spring Boot can be used with Swing apps");
+//        panel.add(text, BorderLayout.CENTER);
+//        frame.setContentPane(panel);
+//        frame.setVisible(true);
 
         if (Desktop.isDesktopSupported()) {
             Desktop desktop = Desktop.getDesktop();
